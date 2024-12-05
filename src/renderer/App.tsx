3d318a491e23f448023ecb592eedcf60c970442f
steve <sylvius4@gmail.com>
2024-12-05 19:28:24 +0900

    add: renderer interface

---
+++ src/renderer/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div style={{ padding: '20px', textAlign: 'center' }}>
+      <h1>💖 Hello from React!</h1>
+      <p>Welcome to your Electron + React application.</p>
+    </div>
+  );
+};
+
+export default App;
